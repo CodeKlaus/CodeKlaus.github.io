a4771fa185cb0b03ed3067afe689bf9aa232a82b
CodeKlaus <me@klaus.link>
2017-06-03 04:08:48 +0800

    Site updated: 2017-06-03 04:08:48

--- js/typing.js
+++ js/typing.js
@@ -11,7 +11,7 @@
                       '</span>')
       }
 
-      $(this).wrap('<a href="' + this.src + '" title="' + alt +
+      $(this).wrap('<a data-fancybox href="' + this.src + '" title="' + alt +
                    '" class="fancybox"></a>')
     })
 
@@ -21,7 +21,6 @@
   })
 
   if ($.fancybox) {
-    $.fancybox.defaults.speed = 60000;
-    $('.fancybox').fancybox()
+    $('[data-fancybox]').fancybox()
   }
 })(jQuery)
